545efd2e3e1b82ea373a0f935028e67189a93dd2
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-08-23 16:18:28 -0400

    web scraper frontend

--- pages/index.js
+++ pages/index.js
@@ -29,17 +29,19 @@ export default function Home() {
       });
 
       const data = await response.json();
-
-      if (data.error) {
-        alert(data.error.message);
-        return;
+      if (response.status !== 200) {
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
 
-      setSubjectLine(data.subjectLine);
-      setResult(data.result);
-    } catch (err) {
-      console.error(err);
-      alert("An unexpected error occurred.");
+      const extractedSubjectLine = data.subjectLine || "Hello from ZooTools :)";
+      setSubjectLine(extractedSubjectLine);
+      setResult(data.result.trim());
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
     }
   }
 
@@ -51,17 +53,15 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to the ZooTools Email Generator
-        </h1>
+        <img src="/zootools.png" className={styles.icon} />
+        <h3>Generate your Email</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
             name="emailGoal"
-            placeholder="Enter your email goal"
+            placeholder="What is the goal of this email?"
             value={emailGoal}
             onChange={(e) => setEmailGoal(e.target.value)}
-            required
           />
           <input
             type="text"
@@ -69,7 +69,6 @@ export default function Home() {
             placeholder="Enter your name"
             value={senderName}
             onChange={(e) => setSenderName(e.target.value)}
-            required
           />
           <input
             type="text"
@@ -77,15 +76,13 @@ export default function Home() {
             placeholder="Enter the recipient's name"
             value={recipientName}
             onChange={(e) => setRecipientName(e.target.value)}
-            required
           />
           <input
-            type="email"
+            type="text"
             name="recipientEmail"
             placeholder="Enter the recipient's email"
             value={recipientEmail}
             onChange={(e) => setRecipientEmail(e.target.value)}
-            required
           />
           <input
             type="text"
@@ -94,19 +91,44 @@ export default function Home() {
             value={websiteUrl}
             onChange={(e) => setWebsiteUrl(e.target.value)}
           />
-          <button type="submit">Generate Email</button>
+          <input type="submit" value="Generate email" />
         </form>
-        {result && (
-          <div className={styles.result}>
-            <h2>{subjectLine}</h2>
-            <pre>{result}</pre>
-          </div>
-        )}
+        <textarea
+          className={styles.result}
+          placeholder='Your email will appear here. You can edit the text directly or click "send email" to edit and send from your default email provider.'
+          value={result}
+          onChange={(e) => setResult(e.target.value)}
+        />
+        <div className={styles.buttonContainer}>
+          <button
+            className={styles.mailButton}
+            onClick={() =>
+              window.open(
+                `mailto:${recipientEmail}?subject=${encodeURIComponent(
+                  subjectLine
+                )}&body=${encodeURIComponent(result)}`,
+                "_blank"
+              )
+            }
+          >
+            Send Email
+          </button>
+          <button
+            className={styles.clearButton}
+            onClick={() => {
+              setEmailGoal("");
+              setSenderName("");
+              setRecipientName("");
+              setRecipientEmail("");
+              setWebsiteUrl(""); // Clearing the website URL field
+              setSubjectLine("Hello From ZooTools");
+              setResult("");
+            }}
+          >
+            Clear Form
+          </button>
+        </div>
       </main>
-
-      <footer className={styles.footer}>
-        <p>ZooTools - Helping businesses grow</p>
-      </footer>
     </div>
   );
 }
