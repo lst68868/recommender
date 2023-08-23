f687e995b3ac7eff85f71ffe629f2be44193ca25
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-08-23 16:44:59 -0400

    scraper error to fix

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -80,11 +80,36 @@ async function scrapeWebsite(url) {
 }
 
 function generatePrompt(emailGoal, websiteContent) {
-  return `Goal: ${emailGoal}
-Website Content: ${websiteContent}
-Subject: [Please insert a subject here]
-Dear [Name],
-[Please write an email that accomplishes the goal stated above.]
-Sincerely,
-[Your Name]`;
+  let promptDetails = "";
+
+  if (
+    emailGoal.includes("waitlist") ||
+    emailGoal.includes("referral") ||
+    emailGoal.toLowerCase().includes("Zootools Acquire")
+  ) {
+    promptDetails +=
+      " Include details about ZooTools Acquire, the #1 tool for creating waitlists and referral programs.";
+  }
+  if (
+    emailGoal.includes("email marketing") ||
+    emailGoal.includes("engagement") ||
+    emailGoal.toLowerCase().includes("Zootools Engage")
+  ) {
+    promptDetails +=
+      " Highlight ZooTools Engage for modern email marketing with built-in virality.";
+  }
+  if (
+    emailGoal.includes("forms") ||
+    emailGoal.includes("user segmentations") ||
+    emailGoal.toLowerCase().includes("Zootools Pandas")
+  ) {
+    promptDetails +=
+      " Mention Zootools Pandas for an alternative to old-fashioned forms and powerful user segmentations.";
+  }
+
+  if (websiteContent) {
+    promptDetails += ` Additionally, consider the following information extracted from the website: ${websiteContent}`;
+  }
+
+  return `You are a marketing assistant at ZooTools, a company that helps businesses grow their users with referral marketing, waitlists, referral programs, and gamified competitions. The user has provided the following goal for an email campaign: "${emailGoal}".${promptDetails} Please craft an engaging email that aligns with this goal and highlights the benefits of ZooTools. Provide a subject for the email with the format "Subject:"`;
 }
