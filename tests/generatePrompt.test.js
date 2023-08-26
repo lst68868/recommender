0d5860b894ae454726f070648fd31ee036751b01
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-08-26 12:25:30 -0400

    fixed tests

--- tests/generatePrompt.test.js
+++ tests/generatePrompt.test.js
@@ -1,11 +1,11 @@
 import { generatePrompt } from "../pages/api/generate"; // Adjust this import to the location of your function
 
 describe("generatePrompt function", () => {
-  it("should include details about ZooTools Acquire for referral goal", () => {
+  it("should include details about ZooTools Acquire for waitlist goal", () => {
     const emailGoal = "waitlist";
     const result = generatePrompt(emailGoal);
     expect(result).toContain(
-      "Include details about ZooTools Acquire, the #1 tool for creating waitlists and referral programs."
+      "Include details about ZooTools Acquire, the leading tool for creating waitlists and referral programs."
     );
   });
 
@@ -13,63 +13,63 @@ describe("generatePrompt function", () => {
     const emailGoal = "referral";
     const result = generatePrompt(emailGoal);
     expect(result).toContain(
-      "Include details about ZooTools Acquire, the #1 tool for creating waitlists and referral programs."
+      "Include details about ZooTools Acquire, the leading tool for creating waitlists and referral programs."
     );
   });
 
-  it("should include details about ZooTools Acquire for referral goal", () => {
-    const emailGoal = "zootools acquire";
+  it("should include details about ZooTools Acquire when goal mentions 'ZooTools Acquire'", () => {
+    const emailGoal = "Zootools Acquire";
     const result = generatePrompt(emailGoal);
     expect(result).toContain(
-      "Include details about ZooTools Acquire, the #1 tool for creating waitlists and referral programs."
+      "Include details about ZooTools Acquire, the leading tool for creating waitlists and referral programs."
     );
   });
 
-  it("should include Highlight ZooTools Engage for modern email marketing with built-in virality.", () => {
+  it("should include details about ZooTools Engage for email marketing goal", () => {
     const emailGoal = "email marketing";
     const result = generatePrompt(emailGoal);
     expect(result).toContain(
-      "Highlight ZooTools Engage for modern email marketing with built-in virality."
+      "Highlight ZooTools Engage for advanced email marketing with integrated virality."
     );
   });
 
-  it("should include Highlight ZooTools Engage for modern email marketing with built-in virality.", () => {
+  it("should include details about ZooTools Engage for engagement goal", () => {
     const emailGoal = "engagement";
     const result = generatePrompt(emailGoal);
     expect(result).toContain(
-      "Highlight ZooTools Engage for modern email marketing with built-in virality."
+      "Highlight ZooTools Engage for advanced email marketing with integrated virality."
     );
   });
 
-  it("should include Highlight ZooTools Engage for modern email marketing with built-in virality.", () => {
+  it("should include details about ZooTools Engage when goal mentions 'ZooTools Engage'", () => {
     const emailGoal = "Zootools Engage";
     const result = generatePrompt(emailGoal);
     expect(result).toContain(
-      "Highlight ZooTools Engage for modern email marketing with built-in virality."
+      "Highlight ZooTools Engage for advanced email marketing with integrated virality."
     );
   });
 
-  it("should include Mention Zootools Pandas for an alternative to old-fashioned forms and powerful user segmentations.", () => {
+  it("should include details about ZooTools Pandas for forms goal", () => {
     const emailGoal = "forms";
     const result = generatePrompt(emailGoal);
     expect(result).toContain(
-      "Mention Zootools Pandas for an alternative to old-fashioned forms and powerful user segmentations."
+      "Mention Zootools Pandas as an upgrade to traditional forms with powerful user segmentation capabilities."
     );
   });
 
-  it("should include Mention Zootools Pandas for an alternative to old-fashioned forms and powerful user segmentations.", () => {
+  it("should include details about ZooTools Pandas for user segmentations goal", () => {
     const emailGoal = "user segmentations";
     const result = generatePrompt(emailGoal);
     expect(result).toContain(
-      "Mention Zootools Pandas for an alternative to old-fashioned forms and powerful user segmentations."
+      "Mention Zootools Pandas as an upgrade to traditional forms with powerful user segmentation capabilities."
     );
   });
 
-  it("should include Mention Zootools Pandas for an alternative to old-fashioned forms and powerful user segmentations.", () => {
+  it("should include details about ZooTools Pandas when goal mentions 'ZooTools Pandas'", () => {
     const emailGoal = "Zootools Pandas";
     const result = generatePrompt(emailGoal);
     expect(result).toContain(
-      "Mention Zootools Pandas for an alternative to old-fashioned forms and powerful user segmentations."
+      "Mention Zootools Pandas as an upgrade to traditional forms with powerful user segmentation capabilities."
     );
   });
 });
